35beb190bd185a7055165866f6c1c22d14c71b84
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-06-11 12:11:52 +0530

    first commit

--- src/profile/Github.jsx
+++ src/profile/Github.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Github = () => {
   return (
-    <a href="https://dribbble.com/" target="_blank" rel="noopener noreferrer">
-                            <span className="socialbtn text-[#e14a84]">
-                                <i className="fa-brands fa-github"></i>
-                            </span>
-                        </a>
-  )
-}
+    <a href="https://github.com/NikhilPathaniaa" target="_blank" rel="noopener noreferrer">
+      <span className="socialbtn text-[#e14a84]">
+        <i className="fa-brands fa-github"></i>
+      </span>
+    </a>
+  );
+};
 
-export default Github
+export default Github;
--- src/profile/LinkedIn.jsx
+++ src/profile/LinkedIn.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const LinkedIn = () => {
   return (
-    <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
-                            <span className="socialbtn text-[#0072b1]">
-                                <i className="fa-brands fa-linkedin-in"></i>
-                            </span>
-                        </a>
-  )
-}
+    <a href="https://www.linkedin.com/in/nikhil-pathania-/" target="_blank" rel="noopener noreferrer">
+      <span className="socialbtn text-[#0072b1]">
+        <i className="fa-brands fa-linkedin-in"></i>
+      </span>
+    </a>
+  );
+};
 
-export default LinkedIn
+export default LinkedIn;
